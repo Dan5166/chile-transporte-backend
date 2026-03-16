c51426e9c2215e512769b16f9d1797ffe98973ef
Danyael Vasquez <d.vsquezojeda@uandresbello.edu>
2026-03-16 21:00:27 +0000

    ADD cambios cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,26 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: (ctx) => {
+        const allowed = [
+          'https://chiletransporte.cl',
+          'https://www.chiletransporte.cl',
+          'http://localhost:5173',
+          'http://localhost:3000'
+        ];
+        if (allowed.includes(ctx.request.header.origin)) {
+          return ctx.request.header.origin;
+        }
+        return false;
+      },
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
